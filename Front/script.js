a89e136c3381400f33971404fbf2fb65b2690942
Wesley3432 <rodrigo.ryuzak@gmail.com>
2025-10-02 11:30:09 -0300

    Add files via upload

--- Front/script.js
+++ Front/script.js
@@ -1,3 +1,37 @@
 const resultsDiv = document.getElementById('results');
 
-console.log(resultsDiv)
+
+ async function SilksongItens(){
+    resultsDiv.innerHTML = "<p>Carregando...<p>";
+    
+    try {
+        const response = await fetch('../Dados/data.json')
+        const data = await response.json()
+        console.log(data);
+
+        if(data.error){
+            resultsDiv.innerHTML = "<p>ERRO AO ACHAR DADOS!<p>";
+            return;
+        }
+
+        resultsDiv.innerHTML = ''
+
+        data.forEach(item => {
+            const card = document.createElement("div");
+            card.className = "card"
+            card.innerHTML = `
+                <img src="${item.imagem}" alt="${item.name}"
+                <h2>${item.name}</h2>
+                <h3>${item.descricao}</h3>
+                <p1>${item.preco.toFixed(2)}</p>
+            `       
+            resultsDiv.appendChild(card);
+        })
+
+    } catch(error) {
+        resultsDiv.innerHTML = "<p>ERRO AO ACHAR ITENS!<p>";
+    }
+
+}
+
+SilksongItens();
